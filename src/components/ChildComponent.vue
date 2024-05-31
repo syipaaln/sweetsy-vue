c9934499d0c31821a8497d1ac07bf9dcc238c100
syipaaln <syipalatipa@gmail.com>
2024-05-31 09:25:45 +0700

    child-parent component

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>{{ message }}</p>
+        <input type="color" v-model="color" @input="changeColor">
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    import { defineProps, defineEmits } from 'vue';
+
+    const props = defineProps({
+        message: {
+            type: String,
+            required: true
+        }
+    });
+
+    const emit = defineEmits(['colorChanged']);
+
+    const color = ref('#ffffff');
+
+    function changeColor() {
+        emit('colorChanged', color.value);
+    }
+</script>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :style="{ backgroundColor: backgroundColor }">
+        <child-component 
+            :message="parentMessage" 
+            @colorChanged="handleColorChanged"
+        ></child-component>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    import ChildComponent from './ChildComponent.vue';
+
+    const backgroundColor = ref('#ffffff')
+
+    const parentMessage = ref('Ayo ubah warna latar belakang!');
+
+    function handleColorChanged(color) {
+        backgroundColor.value = color;
+    }
+</script>
+  
